8d28cbfadb131bb71937737336550f8d6fb91e53
crisMtech95 <cmg.95.col@hotmail.com>
2021-07-19 19:18:33 -0400

    home page

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { useSelector } from "react-redux"
 
 const HomePage = () => {
-
+    const accessT = useSelector(state => state.access.access_token)
 
     return (
         <div className="homePage__container">
