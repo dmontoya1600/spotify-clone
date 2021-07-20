c0b36711d09cd3377dea955dcfbbc23172ac089c
crisMtech95 <cmg.95.col@hotmail.com>
2021-07-19 22:16:53 -0400

    css files and playlist component

---
+++ react-app/src/components/Playlist/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+
+export default function Playlist() {
+    return (
+        <div className="playlist__mainContainer">
+            <div className="playlist__container">
+                <img alt="You'll never know" src="sdpg" className="playlist__image"
+            </div>
+        </div>
+    )
+}
---
+++ react-app/src/components/Song/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+
+export default function Song() {
+    return (
+        <div className="song__container">
+
+            
+        </div>
+    )
+}
