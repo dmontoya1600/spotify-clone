4f7167f6e480a480654f59ec4ef95413beabfda0
crisMtech95 <cmg.95.col@hotmail.com>
2021-07-21 19:54:34 -0400

    homePage f playlists

--- react-app/src/components/MainContent/index.js
+++ react-app/src/components/MainContent/index.js
@@ -7,7 +7,7 @@ import "./MainContent.css"
 
 
 const MainContent = () => {
-    const [activeComp, setActiveComp] = useState("Search")
+    const [activeComp, setActiveComp] = useState("HomePage")
 
     // useEffect(() => {
 
