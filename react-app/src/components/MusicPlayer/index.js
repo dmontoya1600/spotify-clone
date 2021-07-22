4ca5e25624edb612293b2b11b6d9a7bfeba0d086
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-07-22 17:01:40 -0500

    simport SpotifyPlayer and render to MusicPlayer component

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import "./MusicPlayer.css"
+import "./MusicPlayer.css";
+import SpotifyPlayer from 'react-spotify-player';
 
 
 const MusicPlayer = () => {
-
-
+    
+    const size = {
+    width: '100%',
+    height: 300,
+  };
+  
+  const view = 'list'; // or 'coverart'
+  const theme = 'black'; // or 'white'
+   
     return (
         <div className="MusicPlayer__container">
-    
+            <SpotifyPlayer
+            uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
+            size={size}
+            view={view}
+            theme={theme}
+            />
         </div>
     )
 }
