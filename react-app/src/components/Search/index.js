1960d692a2a2e8b237827355f4d5081958620aa2
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-07-21 23:40:38 -0500

    restructure search page

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -3,24 +3,31 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import "./Search.css"
 import Song from '../Song';
-
+import MusicPlayer from "../MusicPlayer";
+import Main from "../Main";
+import NavBar from "../NavBar/NavBar"
+import SideBar from '../SideBar';
 
 const Search = () => {
     const token = useSelector(state => state.access.access_token)
     const searchRes = useSelector(state => state.searchRes.tracks)
-    // console.log(searchRes)
 
+    const [song, setSong] = useState("");
+    
     return (
-        <div className="Search__container">
-            <div className="search__title">
-                <h2>Songs</h2>
-            </div>
-            <div className="Search__songsContainer">
-                {searchRes?.items?.map(track => (
-                    <Song key={track.id} track={track} />
-                ))}
+    <div className="Main__container">
+        <NavBar/>
+        <SideBar/>
+        <div className="MainContent__container">      
+        <div className="Search__songsContainer">
+            {searchRes?.items?.map(track => (
+                <Song key={track.id} track={track} />
+            ))}
             </div>
         </div>
+    <MusicPlayer/>
+    </div>
+      
     )
 }
 
