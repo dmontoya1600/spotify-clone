045e9d55e5cd5f5f290c9264ae2445dbfc9fa741
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-07-23 16:14:54 -0500

    add default song

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -11,21 +11,32 @@ const MusicPlayer = () => {
     const size = {
     width: '100%',
     height: 300,
-  };
+};
   
-  const view = 'list'; // or 'coverart'
-  const theme = 'black'; // or 'white'
-   
-    return (
-        <div className="MusicPlayer__container">
-            <SpotifyPlayer
-            uri={`spotify:track:${currentSong}`}
-            size={size}
-            view={view}
-            theme={theme}
-            />
-        </div>
-    )
+  const view = 'coverart';
+  const theme = 'black';
+
+  if (currentSong) {
+      return (<div className="MusicPlayer__container">
+      <SpotifyPlayer
+      uri={`spotify:track:${currentSong}`}
+      size={size}
+      view={view}
+      theme={theme}
+      /> 
+  </div>
+  )}
+
+  return (
+    <div className="MusicPlayer__container">
+    <SpotifyPlayer
+    uri={`spotify:track:60ynsPSSKe6O3sfwRnIBRf`}
+    size={size}
+    view={view}
+    theme={theme}
+    />
+    </div>
+  )
 }
 
 export default MusicPlayer
