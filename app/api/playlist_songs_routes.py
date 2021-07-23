d119cf54e0b5422d98b21a25803658fa3057dda0
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-07-22 23:22:49 -0500

    add file

--- app/api/playlist_songs_routes.py
+++ app/api/playlist_songs_routes.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, jsonify, session, request
+from flask import Blueprint, request
 from app.models import User, db, Song, Playlist
-from app.forms import LoginForm
-from app.forms import SignUpForm
-from flask_login import current_user, login_user, logout_user, login_required
-
 
 playlist_songs_routes = Blueprint("playlist-songs", __name__)
 
 
 @playlist_songs_routes.route("/", methods=['POST'])
 def get_songs():
-    id = request.json['playlistId']
-    songs = Playlist.query.find_by(id).songs.all()
-    print("STUFFF: ", songs)
-    return None
+    playlistId = request.json["playlistId"]
+    playlist = Playlist.query.get(playlistId)
+    songs = [{"id": song.id, "api_id": song.api_id, "artist_name": song.artist_name, "song_name": song.song_name, "image_url": song.image_url} for song in playlist.songs]
+    # print("OBJECTOIDS", songs)
+    return {"songs": songs}
+
+# {song.id, song.api_id, song.artist_name,
+            #   song.song_name, song.image_url} 
