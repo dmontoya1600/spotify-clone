77a7b32d1167ece96bab3a032ebc4294b63e72c4
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-07-25 00:59:44 -0500

    logout fix

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
-  const user = useSelector(state => state.session.user)
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user);
+  const history = useHistory();
 
   const onLogout = async (e) => {
     await dispatch(logout());
+    return history.push("/");
   };
 
   return <button id="logoutButton" onClick={onLogout}>Logout</button>;
