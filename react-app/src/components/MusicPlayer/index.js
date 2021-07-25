4e906d49b9dd502d4584a0cf1fc27ca8cd963a35
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-07-25 01:47:01 -0500

    fix surplus code in audio player

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -5,20 +5,12 @@ import "./MusicPlayer.css";
 
 const MusicPlayer = () => {
 
-    const {currentSong} = useSelector(state => state.currentSong)
-    const user = useSelector(state => state.session.user)
-    
-    const size = {
-    width: '100%',
-    height: 300,
-};
-  
-  const view = 'coverart';
-  const theme = 'black';
+  const {currentSong} = useSelector(state => state.currentSong)
+  const user = useSelector(state => state.session.user)
 
   if (currentSong && user) {
       return (<div className="MusicPlayer__container">
-  <iframe src={`https://open.spotify.com/embed/${currentSong}`} view="list" width="100%" height="100%" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  <iframe src={`https://open.spotify.com/embed/${currentSong}`} width="100%" height="80px" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
   )} else {
     return <div className="MusicPlayer__container">
